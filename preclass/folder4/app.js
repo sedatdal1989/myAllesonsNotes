459a4dba42f38d371579bbe4f335f26d5de01db4
sedatdal1989 <sedatdl2746@gmail.com>
2024-02-13 19:29:44 +0100

    son calisma

--- preclass/folder4/app.js
+++ preclass/folder4/app.js
@@ -2,5 +2,19 @@ var sedat = document.getElementById("div_1")
 var sedat2 = document.getElementById("div_2")
 
 sedat.onclick = function() {
-    sedat.style.back
+    sedat.style.backgroundColor = "red";
+    sedat2.style.width = "70%";
+    
+    
+}
+sedat2.onclick = function(){
+    sedat.style.backgroundColor = "pink";
+
+}
+sedat.onmouseenter = function(){
+    sedat.style.width = "45%";
+
+}
+sedat2.onmouseout = function(){
+    sedat2.style.backgroundColor = "brown";
 }
